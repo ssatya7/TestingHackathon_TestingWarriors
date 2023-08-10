0f4d162cb8415df36043fbbf6d3645d17aca79da
ssatya7 <Satya_Nuvvula@epam.com>
2023-08-10 18:04:23 +0530

    Removed all unwanted code

--- src/main/java/hcp/pages/BasePage.java
+++ src/main/java/hcp/pages/BasePage.java
@@ -1,4 +1,4 @@
-package web.ui.pages;
+package hcp.pages;
 
 import com.codeborne.selenide.impl.SelenidePageFactory;
 
--- src/main/java/web/ui/pages/LoginPage.java
+++
@@ -1,9 +0,0 @@
-package web.ui.pages;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-
-public class LoginPage extends BasePage {
-    private final Logger LOGGER = LogManager.getLogger(LoginPage.class);
-}
